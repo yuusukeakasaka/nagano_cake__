f0626577d22e26ff16b86402aa903d5e7d252074
EC2 Default User <ec2-user@ip-172-31-64-208.ec2.internal>
2021-08-23 10:31:38 +0000

    [Add]admin/top

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   devise_for :admin
 
     namespace :admin do
+    root :to => "homes#top"
     resources :items
+    resources :customers, only:[:index, :show, :edit, :update]
   end
 end
