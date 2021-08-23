9ac175de5d586d2d8d99acf9b4e649c20896f9c7
EC2 Default User <ec2-user@ip-172-31-64-208.ec2.internal>
2021-08-23 12:15:53 +0000

    [Add]admin/custmersのindex.html.erb

--- app/controllers/admin/customers_controller.rb
+++ app/controllers/admin/customers_controller.rb
@@ -1,15 +1,28 @@
 class Admin::CustomersController < ApplicationController
+  before_action :authenticate_admin!
 
   def index
+    @customers = Customer.all
   end
 
   def show
+    @customer = Customer.find(params[:id])
   end
 
   def edit
+    @customer = Customer.find(params[:id])
+    @customers.update(customer_params)
+    redirect_to admin_customers_path(@customer.id)
   end
 
   def update
   end
+
+    private
+
+  def customer_params
+    params.require(:customer).permit(:last_name, :first_name, :last_name_kana, :first_name_kana, :email, :encrypted_password, :postal_code, :address, :telephone_number, :is_active, :created_at, :updated_at)
+  end
+
 
 end
