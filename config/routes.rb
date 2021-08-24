e989b53f017a96ef040e3f4031af1ff30c682614
EC2 Default User <ec2-user@ip-172-31-64-208.ec2.internal>
2021-08-24 12:02:03 +0000

    [Add]topページ

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,13 @@
 Rails.application.routes.draw do
+  root :to => "homes#top"
+  get 'homes/about'
   devise_for :admin
+
+
+
+   namespace :public do
+
+    end
 
     namespace :admin do
     root :to => "homes#top"
