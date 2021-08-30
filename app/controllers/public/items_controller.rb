8d13b0043781c94750c478f5f9f34011735b4410
EC2 Default User <ec2-user@ip-172-31-64-208.ec2.internal>
2021-08-30 10:36:36 +0000

    [Update]top.html.erb

---
+++ app/controllers/public/items_controller.rb
@@ -0,0 +1,8 @@
+class Public::ItemsController < ApplicationController
+
+  def show
+  end
+
+  def index
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,7 @@ Rails.application.routes.draw do
     devise_for :customers
     root :to => "homes#top"
     get 'homes/about'
+    resources :items, only:[:show, :index]
     resources :customers, only:[:show]
    end
 
