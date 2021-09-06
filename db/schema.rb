08301cc1cfdbcd7031ef4730c2a43dcab9bee2b7
yuusukeakasaka <ayazato2206a@gmail.com>
2021-09-06 02:55:46 +0000

    [Add]order_itemのモデル

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_09_05_115259) do
+ActiveRecord::Schema.define(version: 2021_09_06_024909) do
 
   create_table "addresses", force: :cascade do |t|
     t.integer "customer_id"
@@ -68,6 +68,15 @@ ActiveRecord::Schema.define(version: 2021_09_05_115259) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "image_id"
+  end
+
+  create_table "order_items", force: :cascade do |t|
+    t.integer "item_id"
+    t.integer "order_id"
+    t.integer "price"
+    t.integer "amount"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
