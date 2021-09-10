2bdd7e5d308211c782caca948d56aaa421761acf
yuusukeakasaka <ayazato2206a@gmail.com>
2021-09-10 12:10:23 +0000

    ordersルーティング

--- config/routes.rb
+++ config/routes.rb
@@ -12,6 +12,8 @@ Rails.application.routes.draw do
     patch "/customers/withdrawl" => "customers#withdrawl", as: 'customers_withdrawl'
     get "/customers/edit" => "customers#edit"
     get 'homes/about'
+    get "orders/confirm" => "orders#confirm", as: 'orders_confirm'
+    get "orders/thanks" => "orders#thanks", as: 'orders_thanks'
     resources :orders, only:[:new, :create, :index, :show]
     resources :items, only:[:show, :index]
     resources :customers, only:[:edit, :update]
