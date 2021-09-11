6c1eaa7764108b27b9053599af61def1213b8441
yuusukeakasaka <ayazato2206a@gmail.com>
2021-09-11 05:58:05 +0000

    新規登録画面

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -1,8 +1,15 @@
 class Public::OrdersController < ApplicationController
-  
+
   def new
     @order = Order.new
     @address = current_customer.addresses
   end
-  
+
+  private
+
+  def order_params
+    params.require(:order).permit(:postal_code, :address, :name, :payment_method, :total_payment,:postage,:status)
+  end
+
+
 end
