39973a3dc308e1764abe46144724e719a620fede
yuusukeakasaka <ayazato2206a@gmail.com>
2021-09-15 09:59:16 +0000

    [Finish]

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -12,7 +12,7 @@ class ApplicationController < ActionController::Base
 
   def after_sign_out_path_for(resource_or_scope)
       if resource_or_scope == :admin
-        new_admin_session_path
+        admin_session_path
       elsif resource_or_scope == :customer
         root_path
       end
